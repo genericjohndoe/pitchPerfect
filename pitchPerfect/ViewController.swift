a30d538ac476eecfe54b4cd5c2f45728a7c4c389
genericjohndoe <j.robinson.johnson21@gmail.com>
2017-05-02 22:48:43 -0500

    Initial Commit

---
+++ pitchPerfect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pitchPerfect
+//
+//  Created by joel johnson on 5/2/17.
+//  Copyright © 2017 joel johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
